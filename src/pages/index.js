715bce097079fbb890621fbedb3ae958faece7b5
Victor Lawrence <victor.lawrence@uga.edu>
2019-03-28 17:08:52 -0400

    Still stupid, but now better

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,17 @@ export default class HomePage extends Component{
             <h1 className="logo__title">Show 'N Tell Live</h1>
             <div>an alternative talent show</div>
             <div>irl + online</div>
+            <br/>
             <div className="logo__links">
               <a href='https://www.youtube.com/channel/UCm-wBSAXZnHofyUVU5vHfNg'> youtube </a>
+              <br/>
               <a href='https://www.twitch.tv/showntelllive'> twitch </a>
+              <br/>
               <a href='https://www.instagram.com/atlshowntell/'> insta </a>
+              <br/>
               <a href='https://www.facebook.com/atlshowntell'> fb </a>
+              <br/>
+              <br/>
               <span role="img" aria-label='red heart'>️️❤️</span>
             </div>
           </div>
